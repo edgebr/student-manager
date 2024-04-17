8aa5dc9bce4292613d7461391edfb72ecff16f49
Filipe Ferreira Figueredo Soriano Pinto <130411621+filipeferreirasoriano@users.noreply.github.com>
2024-04-17 10:46:28 -0300

    Feat: block access from unauthorized users (#14)

--- src/main/java/com/academy/edge/studentmanager/models/User.java
+++ src/main/java/com/academy/edge/studentmanager/models/User.java
@@ -85,7 +85,7 @@ public class User implements UserDetails {
     }
 
     @Override
-    public boolean isEnabled() {
-        return true;
+    public boolean isEnabled(){
+        return !deleted;
     }
 }
