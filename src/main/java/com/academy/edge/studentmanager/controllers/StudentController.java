78a4d8ff837156928ebc3f401060cb34de08e421
ricardovinicius <108153768+ricardovinicius@users.noreply.github.com>
2024-05-09 10:31:12 -0300

    fix: removing /me/ endpoints and centralizing in the email reference endpoints for updates and retrieve

--- src/main/java/com/academy/edge/studentmanager/controllers/StudentController.java
+++ src/main/java/com/academy/edge/studentmanager/controllers/StudentController.java
@@ -9,11 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -52,7 +49,7 @@ public class StudentController {
     }
 
     @PutMapping({"/{email}"})
-    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
+    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR') or authentication.name == #email")
     public ResponseEntity<StudentResponseDTO> updateStudentByEmail(@PathVariable String email,
                                                      @RequestBody @Valid StudentUpdateDTO studentUpdateDTO) {
         StudentResponseDTO studentResponseDTO = studentService.updateStudent(email, studentUpdateDTO);
@@ -61,49 +58,11 @@ public class StudentController {
     }
 
     @PutMapping({"/{email}/photo"})
-    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
+    @PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR') or authentication.name == #email")
     public ResponseEntity<StudentResponseDTO> updateStudentPhotoByEmail(@PathVariable String email,
                                                                         @RequestParam("file") MultipartFile file) {
         StudentResponseDTO studentResponseDTO = studentService.updateStudentPhoto(email, file);
 
         return new ResponseEntity<>(studentResponseDTO, HttpStatus.OK);
-    }
-
-
-    @GetMapping("/me")
-    public ResponseEntity<StudentResponseDTO> getCurrentStudent() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            String email = authentication.getName();
-            StudentResponseDTO studentResponseDTO = studentService.getStudentByEmail(email);
-            return new ResponseEntity<>(studentResponseDTO, HttpStatus.OK);
-        } else {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No student logged in");
-        }
-    }
-
-    @PutMapping("/me")
-    public ResponseEntity<StudentResponseDTO> updateCurrentStudent(@RequestBody @Valid StudentUpdateDTO studentUpdateDTO) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            String email = authentication.getName();
-            StudentResponseDTO studentResponseDTO = studentService.updateStudent(email, studentUpdateDTO);
-            return new ResponseEntity<>(studentResponseDTO, HttpStatus.OK);
-        } else {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No student logged in");
-        }
-    }
-
-    @PutMapping("/me/photo")
-    public ResponseEntity<StudentResponseDTO> updateCurrentStudentPhoto(
-            @RequestParam("file") MultipartFile file) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            String email = authentication.getName();
-            StudentResponseDTO studentResponseDTO = studentService.updateStudentPhoto(email, file);
-            return new ResponseEntity<>(studentResponseDTO, HttpStatus.OK);
-        } else {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No student logged in");
-        }
     }
 }
